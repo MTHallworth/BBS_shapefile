1522620a62cc10ae1cd497dddcff32900dcec033
MTHallworth <mthallworth@gmail.com>
2015-07-30 20:54:20 -0400

    updated data

--- R/BBSmap.R
+++ R/BBSmap.R
@@ -4,17 +4,16 @@
 #'@return returns a shapefile with relative abundance information; 
 #'if more than one alpha code is supplied returns a list of shapfiles named
 #'with the alpha code
-#'@usage data(NAbirdsList)
+#'@usage data(BirdList)
 #'@export
 BBSmap<-function(SpeciesCode){
- Birds<-data(NAbirdsList)
-  #Birds<-read.csv("data/NAbirdsList.csv",header=TRUE)
+ Birds<-data(BirdList)
   x<-SpeciesCode
   y<-destfile<-exdir<-rep(NA,length(x))  
   shapes<-vector('list',length(x))
   for(i in 1:length(x)){
-    if(x[i] %in% Birds[,4]==FALSE) print("Alpha Code not in list")
-    y[i]<-sprintf("%05d",Birds[grep(Birds[,4],pattern=x[i]),2])   
+    if(x[i] %in% Birds[,2]==FALSE) print("Alpha Code not in list")
+    y[i]<-sprintf("%05d",Birds[grep(Birds[,2],pattern=x[i]),1])   
     # Download the shapefile to working directory with name CODE.zip#
     destfile[i]<-paste(getwd(),"/",x[i],".zip",sep="")
     download.file(paste("http://www.mbr-pwrc.usgs.gov/bbs/ra12/ra",y[i],".zip",sep=""),destfile=destfile[i],mode="wb")
