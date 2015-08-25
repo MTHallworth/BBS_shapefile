2afa5cfaa9c9b1546df256aeddd4cca8066a8b97
MTHallworth <mthallworth@gmail.com>
2015-08-24 23:53:29 -0400

    Raster output default

--- R/BBSmap.R
+++ R/BBSmap.R
@@ -6,11 +6,11 @@
 #'with the alpha code
 #'@usage data(Birds)
 #'@export
-BBSmap<-function(SpeciesCode,raster=FALSE){
-  Birds<-data(NAbirdsList)
+BBSmap<-function(SpeciesCode,MakeRaster=TRUE){
+  data(Birds)
   x<-SpeciesCode
   y<-destfile<-exdir<-rep(NA,length(x))  
-  shapes<-exentShape<-rastShape<-rastEmpty<-vector('list',length(x))
+  shapes<-extentShape<-rastShape<-rastEmpty<-vector('list',length(x))
   for(i in 1:length(x)){
     if(x[i] %in% Birds[,4]==FALSE) print("Alpha Code not in list")
     y[i]<-sprintf("%05d",Birds[grep(Birds[,4],pattern=x[i]),2])   
@@ -20,7 +20,8 @@ BBSmap<-function(SpeciesCode,raster=FALSE){
     exdir[i]<-paste(getwd(),"/",x[i],"_unzip",sep="")
     unzip(paste(x[i],".zip",sep=""),exdir=exdir[i])
     shapes[[i]]<-raster::shapefile(paste(exdir[i],"/","ra",y[i],".shp",sep=""))
-    extentShape[[i]]<-raster::extent(Shapes[[i]])
+    if(MakeRaster==FALSE){break} else {
+    extentShape[[i]]<-raster::extent(shapes[[i]])
     rastEmpty[[i]]<-raster::raster(xmn=extentShape[[i]][1],
                                    xmx=extentShape[[i]][2],
                                    ymn=extentShape[[i]][3],
@@ -28,9 +29,14 @@ BBSmap<-function(SpeciesCode,raster=FALSE){
                                    crs=("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"),
                                    resolution=c(10000,10000)) #10km res
     print(paste("Creating Raster for",x[i],sep=" "))
-    rastShape[[i]]<-raster::rasterize(shapes[[i]],rasterEmpty[[i]],shapes[[i]]$RASTAT)                                 
+    rastShape[[i]]<-raster::rasterize(shapes[[i]],rastEmpty[[i]],shapes[[i]]$RASTAT)                                 
+    } 
   }
-  names(shapes)<-c(x)
-  if(length(rastShape)==1) return(rastShape[[1]]) else return(rastShape)
-  
-}
+  if(MakeRaster==FALSE){names(shapes)<-c(x)
+                        if(length(shapes)==1){ return(shapes[[1]])} else { return(shapes)}}
+  else{
+  names(rastShape)<-c(x)
+  if(length(rastShape)==1){ return(rastShape[[1]])} else {return(rastShape)}
+  } 
+  }
+
